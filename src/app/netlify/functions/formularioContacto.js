e237eaccf467a7351f254eeb04c3b015ebd04f37
ialmanza <110355705+ialmanza@users.noreply.github.com>
2024-05-07 22:42:37 -0300

    agregada funcion vacia

---
+++ src/app/netlify/functions/formularioContacto.js
@@ -0,0 +1,48 @@
+// export.handler = async (event, context) => {
+//   const { name, email, message } = JSON.parse(event.body);
+//   const body = JSON.stringify({ name, email, message });
+//   const response = await fetch('https://api.netlify.com/api/v1/forms/5b6a9b1e-6a0e-4c0b-a6c9-7a0d9b9c7c0d/submissions', {
+//     method: 'post',
+//     headers: {
+//       'Content-Type': 'application/json',
+//       'Authorization': `Bearer ${process.env.NETLIFY_API_KEY}`
+//     },
+//     body
+//   });
+//   return {
+//     statusCode: 405,
+//     message: 'Metodo no soportado',
+//   }
+// }
+
+exports.handler = async (event, context) => {
+      switch (event.httpMethod) {
+        case 'POST':
+          const params = JSON.parse(event.body);
+          console.log('RECIBI UNA SOLICITUD',params);
+        //   const { name, email, message } = JSON.parse(event.body);
+        //   const body = JSON.stringify({ name, email, message });
+        //   const response = await fetch('https://api.netlify.com/api/v1/forms/5b6a9b1e-6a0e-4c0b-a6c9-7a0d9b9c7c0d/submissions', {
+        //     method: 'post',
+        //     headers: {
+        //       'Content-Type': 'application/json',
+        //       'Authorization': `Bearer ${process.env.NETLIFY_API_KEY}`
+        //     },
+        //     body
+        //   });
+        //   return {
+        //     statusCode: 200,
+        //     body: JSON.stringify({ message: 'Formulario enviado correctamente' }),
+        //   };
+        // default:
+        //   return {
+        //     statusCode: 405,
+        //     body: JSON.stringify({ message: 'Metodo no soportado' }),
+        //   };
+      default:
+        return {
+          statusCode: 405,
+          body: JSON.stringify({ message: 'Metodo no soportado' }),
+        };
+      }
+  }
