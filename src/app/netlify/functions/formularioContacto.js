c877049da1e3e1a5e154394c4aaf55d02c4f3a38
ialmanza <110355705+ialmanza@users.noreply.github.com>
2024-05-09 00:16:19 -0300

    se agrega una funcion de netlify

--- src/app/netlify/functions/formularioContacto.js
+++ src/app/netlify/functions/formularioContacto.js
@@ -20,29 +20,11 @@ exports.handler = async (event, context) => {
         case 'POST':
           const params = JSON.parse(event.body);
           console.log('RECIBI UNA SOLICITUD',params);
-        //   const { name, email, message } = JSON.parse(event.body);
-        //   const body = JSON.stringify({ name, email, message });
-        //   const response = await fetch('https://api.netlify.com/api/v1/forms/5b6a9b1e-6a0e-4c0b-a6c9-7a0d9b9c7c0d/submissions', {
-        //     method: 'post',
-        //     headers: {
-        //       'Content-Type': 'application/json',
-        //       'Authorization': `Bearer ${process.env.NETLIFY_API_KEY}`
-        //     },
-        //     body
-        //   });
-        //   return {
-        //     statusCode: 200,
-        //     body: JSON.stringify({ message: 'Formulario enviado correctamente' }),
-        //   };
-        // default:
-        //   return {
-        //     statusCode: 405,
-        //     body: JSON.stringify({ message: 'Metodo no soportado' }),
-        //   };
-      default:
-        return {
-          statusCode: 405,
-          body: JSON.stringify({ message: 'Metodo no soportado' }),
+          return;
+        default:
+          return {
+            statusCode: 405,
+            body: JSON.stringify({ message: 'Metodo no soportado' }),
         };
       }
   }
