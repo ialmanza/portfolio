130a0d89144372310bce6f0a5ac09bbf9d3f8a22
ialmanza <110355705+ialmanza@users.noreply.github.com>
2024-06-02 15:07:50 -0300

    proyecto agregado

--- src/app/components/proyectos/proyectos.component.ts
+++ src/app/components/proyectos/proyectos.component.ts
@@ -16,7 +16,11 @@ export class ProyectosComponent {
     window.open('https://ingebilab.netlify.app/', '_blank');
   }
 
-  onImageClick() {
+  todoapp() {
+    window.open('https://tareasparahacer.netlify.app/', '_blank');
+  }
+
+  onImageClickIngebilab() {
     this.isImageZoomed =!this.isImageZoomed;
     const imageElement = document.getElementById('ingebilabImage') as HTMLElement;
     if (this.isImageZoomed) {
@@ -26,8 +30,21 @@ export class ProyectosComponent {
     }
   }
 
+  onImageClickToDoApp() {
+    this.isImageZoomed =!this.isImageZoomed;
+    const imageElement = document.getElementById('todoappImage') as HTMLElement;
+    if (this.isImageZoomed) {
+      imageElement.classList.add('zoomed');
+    } else {
+      imageElement.classList.remove('zoomed');
+    }
+  }
   verCodigoMyLab() {
     window.open('https://github.com/ialmanza/My_Lab', '_blank');
+  }
+
+  verCodigoToDoApp() {
+    window.open('https://github.com/ialmanza/task_manager', '_blank');
   }
 }
 
