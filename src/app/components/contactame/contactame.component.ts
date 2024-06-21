692c6ba7aa7870bbe26d0da909e8094943a4dc5e
ialmanza <isra.almanzagonzalez@gmail.com>
2024-06-21 01:06:44 -0300

    formulario de contacto

--- src/app/components/contactame/contactame.component.ts
+++ src/app/components/contactame/contactame.component.ts
@@ -1,14 +1,47 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import emailjs from '@emailjs/browser';
+
+
+
 @Component({
   selector: 'app-contactame',
   standalone: true,
-  imports: [ ],
+  imports: [ ReactiveFormsModule],
   templateUrl: './contactame.component.html',
   styleUrl: './contactame.component.css'
 })
 export class ContactameComponent {
+  contactForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
 
-  constructor() { }
+    this.contactForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phoneNumber: ['', Validators.required],
+      message: ['', Validators.required]
+    });
+  }
 
+  onSubmit() {
+    console.log(this.contactForm.value);
+  }
+  async send(){
+    emailjs.init('okEjA9dn2w_9EpoeX');
+    let response = await emailjs.send("service_hnxkumu","template_h7iuj6w",{
+      from_name: this.contactForm.value.firstName,
+      to_name: "pocholo",
+      firstName: this.contactForm.value.firstName,
+      lastName: this.contactForm.value.lastName,
+      message: this.contactForm.value.message,
+      phone: this.contactForm.value.phoneNumber,
+      });
+
+      alert("El mensaje se envió correctamente");
+      this.contactForm.reset();
+
+  }
 }
