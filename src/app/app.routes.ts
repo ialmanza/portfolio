8bdc30168b9704bbcb8329989efff59b5790a613
ialmanza <isra.almanzagonzalez@gmail.com>
2025-06-24 23:24:24 -0300

    feature/experience

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
 import { PrincipalComponent } from './components/principal/principal.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ExperienceComponent } from './components/experience/experience.component';
 
 export const routes: Routes = [
   { path: 'sobre-mi', component: SobreMiComponent },
   { path: 'proyectos', component: ProyectosComponent },
   { path: 'contactame', component: ContactameComponent },
   { path: 'footer', component: FooterComponent },
+  { path: 'experience', component: ExperienceComponent },
   { path: '', component: PrincipalComponent }
 ];
---
+++ src/app/components/experience/experience.component.ts
@@ -0,0 +1,41 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FooterComponent } from "../footer/footer.component";
+
+@Component({
+  selector: 'app-experience',
+  standalone: true,
+  imports: [CommonModule, FooterComponent],
+  templateUrl: './experience.component.html',
+  styleUrl: './experience.component.css'
+})
+export class ExperienceComponent {
+experiences = [
+    {
+      title: 'Desarrollador Back-End',
+      company: 'Fyself SL, Abr 2025 - Actualidad',
+      description: 'Desde abril de 2025 me desempeño como desarrollador back-end en Fyself, una empresa tecnológica con una arquitectura basada en microservicios. Participo activamente en el desarrollo y mantenimiento de servicios Back-End escritos en Java con Spring Boot, WebFlux, WebSockets y Python con Flask. Trabajo con una base de código compleja y distribuida en más de 16 microservicios, colaborando con otros equipos y siguiendo buenas prácticas de integración continua, control de versiones y documentación técnica. Me involucro tanto en la implementación de nuevas funcionalidades como en el análisis y mejora de servicios ya existentes.'
+    },
+    {
+      title: 'Investigador Adscripto SCA',
+      company: 'Universidad de Belgrano, Sept 2024 - Actualidad ',
+      description: 'Como desarrollador del proyecto SCA en la Universidad de Belgrano y en calidad de Investigador Adscripto, lidero el diseño e implementación completa del sistema utilizando Angular 18, TailwindCSS, Material Design y Bootstrap. Desarrollé interfaces profesionales, visualizaciones dinámicas con D3.js, autenticación y gestión de datos con Supabase, e internacionalización con ngx-translate en tres idiomas, Español, Inglés y Portugés. Además, colaboro en la investigación y aplicación de buenas prácticas en ingeniería de software, contribuyendo al desarrollo académico y tecnológico del proyecto.'
+    },
+    {
+      title: 'Desarrollador Full Stack',
+      company: 'Clandestino 2024 - 2025',
+      description: 'Desarrollé un sistema de inventario para la gestión de productos, abarcando el diseño de interfaces con Angular y la implementación de funcionalidades CRUD. Creé endpoints con Node.js y Express, integrando MongoDB para garantizar la persistencia y consistencia de los datos. Diseñé tablas dinámicas con filtrado avanzado y aseguré una experiencia de usuario eficiente mediante pruebas y optimizaciones continuas.'
+    },
+    {
+      title: 'Desarrollador de software',
+      company: 'Software de Centro de Recreación, Nov 2023 - Ene 2024',
+      description: 'Formé parte de un equipo de desarrollo de un sistema de gestión para un centro de recreación, donde se implementaron funciones clave como la creación de ventas, gestión de pagos y visualización de productos. Los usuarios podían agregar productos a un carrito de compras y realizar transacciones de manera eficiente. Utilicé un stack tecnológico compuesto por Angular, HTML, CSS, Bootstrap, JavaScript y TypeScript durante un período de tres meses.'
+    }
+  ];
+
+  goBack(): void {
+    // Implementa la lógica de navegación hacia atrás
+    // Por ejemplo: this.router.navigate(['/']);
+    window.history.back();
+  }
+}
